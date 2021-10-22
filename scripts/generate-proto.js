4737bdcdd039dbe55c63ac835c068122126e5fe1
eder lobo <ederandreslobo1998@gmail.com>
2021-10-22 14:02:04 -0500

    adding proto-file generator

---
+++ scripts/generate-proto.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+const pbjs = require("protobufjs/cli").pbjs;
+const pathInput = path.resolve(__dirname, "../koinos-proto/koinos");
+const pathOut = path.resolve(__dirname, "../koinos-proto-js");
+
+function getFiles(dir) {
+  return fs.readdirSync(dir).flatMap((item) => {
+    if(item.endsWith('.proto')) {
+      return "../koinos-proto/koinos"+`${dir}/${item}`.replace(pathInput, "").replace(/\\/g, "/")
+    }    
+    let pathDir = path.resolve(dir, item)
+    if (fs.statSync(pathDir).isDirectory()) {
+      return getFiles(pathDir);
+    }
+  });
+}
+
+async function generate() {
+  let filesProto = await getFiles(pathInput)
+  if (!fs.existsSync(pathOut)) {
+		fs.mkdirSync(pathOut, { recursive: true });
+	}
+
+  pbjs.main([ "--target", "static-module", "--wrap", "commonjs", "--out", "./../koinos-proto-js/index.js" ].concat(filesProto), function(err, output) {
+    if (err) {
+      throw err;
+    }
+    console.log("proto generated")
+  });
+}
+
+generate()
+
